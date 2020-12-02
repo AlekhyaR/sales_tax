96427507e78862606d4c4030f65d97bc20493e68
alekhya <alekhya.rudra@cybrilla.com>
2020-12-02 14:03:12 +0100

    adds specs to line_item

--- lib/line_item.rb
+++ lib/line_item.rb
@@ -1,12 +1,10 @@
 class LineItem
   attr_reader :name, :price, :type, :quantity
-  
   def initialize(name, price, type, quantity)
-    @item_name = name
+    @name = name
     @price = price
     @type = type
     @quantity = quantity
   end
-
   
 end
---
+++ spec/lib/line_item_spec.rb
@@ -0,0 +1,8 @@
+describe LineItem do
+  describe '#initialize' do
+    it 'it determines whether lineitem is imported and sets related attributes' do
+      line_item = LineItem.new('imported box of chocolates', BigDecimal("10"), 'food', 1)
+      expect({name:line_item.name, quantity:line_item.quantity}).to eq({name:'imported box of chocolates',quantity: 1})
+    end
+  end
+end
